7461ddae815c83634d7db0edb07ebddadf84e9e8
sanishjohn <sanishj@gmail.com>
2017-07-31 22:38:32 -0500

    incremental updates

--- bamazonDBandTableDDLs.sql
+++ bamazonDBandTableDDLs.sql
@@ -2,9 +2,25 @@ CREATE DATABASE bamazonDB;
 
 CREATE TABLE bamazonDB.products 
   ( 
-     item_id         INTEGER(11) AUTO_INCREMENT=1001 NOT NULL, 
-     movie      VARCHAR(60) NOT NULL, 
-     five_times BOOLEAN NOT NULL DEFAULT 0, 
-     score      INTEGER(10), 
-     PRIMARY KEY (id) 
+     item_id            INTEGER(11) AUTO_INCREMENT=1001 NOT NULL, 
+     product_name       VARCHAR(256),
+     department_id      INTEGER(11),
+     department_names   VARCHAR(256), 
+     price              DECIMAL(10,2), 
+     stock_quantity     INTEGER(10),
+     PRIMARY KEY (item_id) 
   ); 
+
+  SELECT * FROM bamazonDB.products;
+
+  CREATE TABLE bamazonDB.departments 
+  ( 
+     department_id      INTEGER(11),
+     department_names   VARCHAR(256), 
+     over_head_costs    DECIMAL(10,2), 
+     product_sales      DECIMAL(10,2),
+     total_profit       DECIMAL(10,2)
+     PRIMARY KEY (item_id) 
+  ); 
+
+  SELECT * FROM bamazonDB.departments;
