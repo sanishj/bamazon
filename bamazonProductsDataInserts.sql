4abed92a444ecce4dac363c300da10308e2dee17
sanishjohn <sanishj@gmail.com>
2017-08-04 18:59:18 -0500

    updating sql logics

---
+++ bamazonProductsDataInserts.sql
@@ -0,0 +1,56 @@
+INSERT INTO bamazonDB.products
+VALUES ("Mr. Beams Wireless LED Spotlight", 2000, "electronics", 12.99, 200)
+
+INSERT INTO bamazonDB.products
+VALUES ("Dickies Student Backpack", 2001, "BacktoSchool", 15.99, 150)
+
+INSERT INTO bamazonDB.products
+VALUES ("Cardinal 1.5 inch D-Ring View Binders", 2001, "BacktoSchool", 2.99, 99)
+
+INSERT INTO bamazonDB.products
+VALUES ("Elmer's Liquid School Glue", 2001, "BacktoSchool", 3.39, 199)
+
+INSERT INTO bamazonDB.products
+VALUES ("Dixon Ticonderoga Wood-Cased 2HB Pencils", 2001, "BacktoSchool", 4.29, 500)
+
+INSERT INTO bamazonDB.products
+VALUES ("BamazonBasics D Cell Alkaline Batteries (12-Pack)", 2000, "electronics", 11.39, 200)
+
+INSERT INTO bamazonDB.products
+VALUES ("BamazonBasics 8-Sheet Shredder", 2000, "electronics", 34.99, 21)
+
+INSERT INTO bamazonDB.products
+VALUES ("Syma X11 R/C Quadcopter", 2000, "electronics", 29.99, 11)
+
+INSERT INTO bamazonDB.products
+VALUES ("P.S. from Paris", 2002, "Books", 10.99, 253)
+
+INSERT INTO bamazonDB.products
+VALUES ("The Neon Lawyer", 2002, "Books", 5.99, 101)
+
+INSERT INTO bamazonDB.products
+VALUES ("The Dark Tower I The Gunslinger", 2002, "Books", 10.24, 57)
+
+INSERT INTO bamazonDB.products
+VALUES ("The Late Show", 2002, "Books", 12.17, 111)
+
+INSERT INTO bamazonDB.products
+VALUES ("LEGO Classic Creative Builder Box", 2003, "Toys", 29.99, 151)
+
+INSERT INTO bamazonDB.products
+VALUES ("LEGO Juniors Shark Attack 10739 Building Kit", 2003, "Toys", 18.97, 34)
+
+INSERT INTO bamazonDB.products
+VALUES ("Play-Doh 10-Pack of Colors", 2003, "Toys", 9.99, 51)
+
+INSERT INTO bamazonDB.products
+VALUES ("LeapFrog LeapStart Interactive Learning System", 2003, "Toys", 39.99, 95)
+
+INSERT INTO bamazonDB.products
+VALUES ("Nerf Modulus Recon MKII Blaster", 2003, "Toys", 17.77, 21)
+
+INSERT INTO bamazonDB.products
+VALUES ("Incline Fit Anti-Burst Yoga Exercise Ball", 2004, "Fitness", 18.51, 5)
+
+INSERT INTO bamazonDB.products
+VALUES ("NordicTrack E 7.0 Z Elliptical Trainer", 2004, "Fitness", 599.99, 3)
