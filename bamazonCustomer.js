a86e14257f54a524260ecaa8e638d6dd70d4a940
sanishjohn <sanishj@gmail.com>
2017-08-05 19:27:44 -0500

    incremental update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,22 +7,26 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root", //Your username
-    password: "", //Supply your password here
+    password: "007intel", //Supply your password here
     database: "bamazonDB"
 })
 
 var itemIdPrdData = [];
 
+// constructor function used to create programmers objects
+function Selection(item_id, quantity) {
+  this.item_id = item_id;
+  this.quantity = quantity;
+}
+
 //Display stock by All item_id, product_name price, stock_quantity
 connection.query('SELECT * FROM bamazonDB.products;', function(err, res) {
-	// var answerBasic = new BasicFlashcard(answer.front, answer.back);
-        // basicCardArrayData.push(answerBasic);
-        for (var i = 0; i < res.length; i++) {
+	for (var i = 0; i < res.length; i++) {
             itemIdPrdData.push(res[i].item_id + " | " + res[i].product_name);
-            // console.log(color.green_bg(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity));
         }
         console.log(itemIdPrdData);
         console.log("-----------------------------------");
+        prompt();
         connection.end(); // This ends the connection. it is a delivered function from mysql creator
     });
 
@@ -32,11 +36,42 @@ prompt = function() {
         name: 'option',
         message: color.green_bg('\nWhat product would you like to buy?\n'),
         choices: itemIdPrdData
-    }]).then(function(choice) {
+    }, 
+    {
+    name: "language",
+    message: "What is your favorite programming language?"
+  }]).then(function(choice) {
         console.log(choice.name);
     })
 }
-prompt();
+
+inquirer.prompt([{
+   type: "list",
+   name: "option",
+   message: "What product would you like to buy?",
+   choices: [
+       "Black Purse",
+       "Velvet"
+   ]
+}]).then(function(user) {
+   console.log(user.option);
+
+   if (user.option == 'Black Purse') {
+       inquirer.prompt([{
+           type: 'input',
+           message: 'How many do you want to purchase?',
+           name: 'numberItem'
+       }]).then(function(item) {
+           var post = { ProductName: item.Purse-blk, StockQuanitiy: item.numberItem }
+           con.query('Select * from products * where ProductName set Purse-blk', post, function(err, result) {
+               if (err) throw err;
+           });
+           con.query('SELECT * from products order by ItemID DESC limit 1', function(err, result) {
+               if (err) throw err;
+               console.log(result);
+           });
+       })
+   } 
 
 
 /*
