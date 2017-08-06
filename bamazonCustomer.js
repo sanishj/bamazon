4f6fb66477a5b3f2a7a2d39daf0687b5d72db576
sanishjohn <sanishj@gmail.com>
2017-08-05 19:35:48 -0500

    incremental update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,9 +14,9 @@ var connection = mysql.createConnection({
 var itemIdPrdData = [];
 
 // constructor function used to create programmers objects
-function Selection(item_id, quantity) {
+function Selection(item_id, numberItem) {
   this.item_id = item_id;
-  this.quantity = quantity;
+  this.numberItem = numberItem;
 }
 
 //Display stock by All item_id, product_name price, stock_quantity
@@ -38,41 +38,14 @@ prompt = function() {
         choices: itemIdPrdData
     }, 
     {
-    name: "language",
-    message: "What is your favorite programming language?"
+    type: 'input',
+    name: 'numberItem',
+    message: 'How many do you want to purchase?'
   }]).then(function(choice) {
-        console.log(choice.name);
+      var newGuy = new Programmer(choice.option, choice.numberItem);
+        console.log(newGuy);
     })
 }
-
-inquirer.prompt([{
-   type: "list",
-   name: "option",
-   message: "What product would you like to buy?",
-   choices: [
-       "Black Purse",
-       "Velvet"
-   ]
-}]).then(function(user) {
-   console.log(user.option);
-
-   if (user.option == 'Black Purse') {
-       inquirer.prompt([{
-           type: 'input',
-           message: 'How many do you want to purchase?',
-           name: 'numberItem'
-       }]).then(function(item) {
-           var post = { ProductName: item.Purse-blk, StockQuanitiy: item.numberItem }
-           con.query('Select * from products * where ProductName set Purse-blk', post, function(err, result) {
-               if (err) throw err;
-           });
-           con.query('SELECT * from products order by ItemID DESC limit 1', function(err, result) {
-               if (err) throw err;
-               console.log(result);
-           });
-       })
-   } 
-
 
 /*
 The app should then prompt users with two messages.
